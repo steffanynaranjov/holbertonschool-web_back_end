d140af10cf001bf6fa1f108eb1eeafda8a3f6542
steffanynaranjov <steffany.naranjo@gmail.com>
2021-08-16 15:49:14 -0400

    adding

---
+++ 0x07-Session_authentication/models/user_session.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" user session module
+"""
+from models.base import Base
+
+
+class UserSession(Base):
+    """ User class """
+    def __init__(self, *args: list, **kwargs: dict):
+        """ Constructor method """
+        super().__init__(*args, **kwargs)
+        self.user_id = kwargs.get('user_id')
+        self.session_id = kwargs.get('session_id')
