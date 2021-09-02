7e7c67bd08576cb02deb7e1d4eba7acbc0ed57b2
steffanynaranjov <steffany.naranjo@gmail.com>
2021-09-02 11:49:33 -0400

    fix

--- 0x0C-MySQL_Advanced/3-glam_rock.sql
+++ 0x0C-MySQL_Advanced/3-glam_rock.sql
@@ -1,7 +1,7 @@
--- Old school band
--- lifespan by band and style
-SELECT band_name,
-	ifnull(split, YEAR(CURDATE())) - formed as lifespan
+-- Old school band.
+-- script that lists all bands with Glam
+-- rock as their main style, ranked by their longevity.
+SELECT band_name AS band_name, IFNULL(split, 2020) - IFNULL(formed, 0) AS lifespan
 FROM metal_bands
-WHERE style LIKE "%Glam rock%"
+WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC;
