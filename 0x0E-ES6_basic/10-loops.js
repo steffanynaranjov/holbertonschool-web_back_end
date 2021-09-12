07cd594a3389fe4b3e913a965bd9a46f7d5a4bd9
steffanynaranjov <steffany.naranjo@gmail.com>
2021-09-12 17:09:49 -0400

    update project

---
+++ 0x0E-ES6_basic/10-loops.js
@@ -0,0 +1,8 @@
+export default function appendToEachArrayValue(array, appendString) {
+    const appendValue = [];
+    for (const value of array) {
+      appendValue.push(`${appendString}${value}`);
+    }
+
+    return appendValue;
+  }
---
+++ 0x0E-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,10 @@
+export default function createIteratorObject(report) {
+    const values = [];
+
+    const departmentsList = Object.values(report.allEmployees);
+    for (const value of departmentsList) {
+      values.push(...value);
+    }
+
+    return values;
+  }
